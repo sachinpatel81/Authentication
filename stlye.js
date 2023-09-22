02a559206525535a42a01b2099d255d6bcd30d22
sachinpatel81 <sachipatel029@gmail.com>
2023-09-22 15:39:35 +0530

    commit on 22/09/2023

--- stlye.js
+++ stlye.js
@@ -32,25 +32,41 @@ function checkregisation() {
 
    if (yourname === "") {
       console.log("enter a valid yourname")
+      document.getElementById('yournameError').textContent ="enter a valid yourname"
+      document.getElementById('yournameError').style.color = "red"
 
    }
    else if (username === "") {
       console.log("enter a valid username")
+      document.getElementById('usernameError').textContent ="enter a valid username"
+      document.getElementById('usernameError').style.color = "red"
+
    }
    else if (password === "") {
       console.log("enter a valid password")
+      document.getElementById('passwordError').textContent ="enter a valid password"
+      document.getElementById('passwordError').style.color = "red"
    }
    else if (DOB === "") {
       console.log("enter a valid DOB")
+      document.getElementById('DOBError').textContent ="enter a valid DOB"
+      document.getElementById('DOBError').style.color = "red"
    }
    else if (selectgender === "") {
       console.log("enter a valid gender")
+      document.getElementById('genderError').textContent ="enter a valid gender"
+      document.getElementById('genderError').style.color = "red"
    }
    else if (city === "") {
       console.log("enter a valid city")
+      document.getElementById('cityError').textContent ="enter a valid city"
+      document.getElementById('cityError').style.color = "red"
+   
+
    }
    else if (password.length < 3){
       console.log("enter maximum 3 letter password")
+      document.getElementById('error').textContent ="enter maximum 3 letter password"
    }
    else 
    {
@@ -60,6 +76,8 @@ function checkregisation() {
       console.log(DOB)
       console.log(selectgender)
       console.log(city)
+      document.getElementById('error').textContent =""
+
 
    }
    
